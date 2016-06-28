075eab6a9568a56ef40447cf5a71bec9db2268c6
Philip Gasteiger <philip.gasteiger@gmail.com>
2016-06-28 19:20:01 +0200

    Add heuristic implementation

--- heuristic.py
+++ heuristic.py
@@ -1,43 +1,30 @@
+variables = {1: 'false', 'false': 1}
+holes = []
+pigeons = []
+
+
 def addedVarName(var, name):
-    """
-    Optional.
-    This method is invoked while reading the name associated to a variable.
-    :param var: the id of the variable
-    :param name: the name associated to var
-    """
-    pass
+    global variables, holes, pigeons
+    variables.update({var: name, name: var})
+    if name.startswith("pigeon"):
+        pigeons.append(name[7:-1])
+    if name.startswith("hole"):
+        holes.append(name[5:-1])
 
 
-def onStartingSolver(num_vars, num_clauses):
-    """
-    Optional.
-    When the computation starts (after reading the input and performing the simplifications).
-    :param num_vars: number of vars.
-    :param num_clauses: number of clauses. The ids for variables start from 1.
-    """
-    # set-up interpretation as everything is unknown
-    global interpretation, UNKNOWN
-    interpretation = [UNKNOWN] * (num_vars + 1)
+def onFinishedParsing():
+    # do not remove any variable during simplification
+    return [v for v in variables.keys() if isinstance(v, int)]
 
 
 def choiceVars():
-    """
-    Required.
-    This method is invoked when a choice is needed. It can return a choice, a list of choices and special
-    values for performing special actions.
-    Special values:
-     - [1, 0] force the solver to perform a restart
-     - [n, 2, 0] use the fallback heuristic for n steps (n<=0 use always fallback heuristic) -> require the presence of the method fallback() in the script
-     - [v, 3, 0] unroll the truth value of the variable v
-     - [4, 0] force the solver to stop the computation returning incoherent
-    :return: A list of choices. Be careful, the first choice is the last element in the list.
-    """
-    pass
+    global variables, holes, pigeons
+    if len(pigeons) > len(holes):
+        return [4, 0]  # force incoherence
+    # assign pigeon i to hole i
+    return [variables["inHole(%s,%s)" % (pigeons[i], holes[i])]
+            for i in range(0, len(pigeons) - 1)]
 
 
 def ignorePolarity():
-    """
-    Required if onChoiceContradictory has not been set.
-    This method tells the solver to use the suggested polarity if possible, otherwise the solver infers the polarity in order to have no conflicts. If this method is added than onChoiceContradictory must not be added.
-    """
-    pass
+    pass  # required for choiceVars
